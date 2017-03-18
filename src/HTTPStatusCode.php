beac4fc5a38fdebe2d1eeeae432f2da1a91841b6
Josantonius <info@josantonius.com>
2017-03-18 05:02:59 +0100

    The repository was updated to version 1.1.1

--- src/HTTPStatusCode.php
+++ src/HTTPStatusCode.php
@@ -2,15 +2,11 @@
 /**
  * PHP library to get the meaning from HTTP response status codes.
  * 
- * @category   JST
- * @package    HTTPStatusCode
- * @subpackage HTTPStatusCode
- * @author     Josantonius - info@josantonius.com
- * @copyright  Copyright (c) 2016 JST PHP Framework
+ * @author     Josantonius - hello@josantonius.com
+ * @copyright  Copyright (c) 2016 - 2017
  * @license    https://opensource.org/licenses/MIT - The MIT License (MIT)
- * @version    1.1.0
  * @link       https://github.com/Josantonius/PHP-HTTPStatusCode
- * @since      File available since 1.0.0 - Update: 2017-01-30
+ * @since      1.0.0
  */
 
 namespace Josantonius\HTTPStatusCode;
