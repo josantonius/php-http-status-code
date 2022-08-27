e261015c060536d922dfe073f4454750174d0420
Josantonius <hello@josantonius.dev>
2022-08-27 23:38:27 +0200

    refactor: fix comment alignment
    The alignment of the asterisks in the comments has been fixed.
    
    Closes #3

--- src/Exceptions/UnsupportedLanguageException.php
+++ src/Exceptions/UnsupportedLanguageException.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
-* This file is part of https://github.com/josantonius/php-http-status-code repository.
-*
-* (c) Josantonius <hello@josantonius.dev>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ * This file is part of https://github.com/josantonius/php-http-status-code repository.
+ *
+ * (c) Josantonius <hello@josantonius.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Josantonius\HttpStatusCode\Exceptions;
 
